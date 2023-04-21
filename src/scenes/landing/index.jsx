fc347fc3f0394fea89cb643951a0cb160ba4d3c0
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-04-21 12:28:09 -0400

    Worked on navbar and landing page layout

--- src/scenes/landing/index.jsx
+++ src/scenes/landing/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./index.css";
+import Portrait from "../../assets/vector-portrait.png";
 
 const index = () => {
   return (
-    <div className="landing fullHeight">
-      <div className="container middle">
-        <div className="title-wrapper">
+    <div className="landing">
+      <div className="container middle flex spaceBetween">
+        <div className="title-wrapper flex column flexMiddle">
           <h1 className="title">Kyle Savage</h1>
           <h3 className="subtitle">Frontend Developer</h3>
+        </div>
+        <div className="portrait">
+          <img src={Portrait} alt="Vector portrait" />
         </div>
       </div>
     </div>
