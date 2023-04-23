4bcfc68f02230c921124273ab1c6d08fe5aa2b46
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-04-22 20:53:30 -0400

    minor changes

--- src/scenes/about/index.jsx
+++ src/scenes/about/index.jsx
@@ -14,7 +14,7 @@ const index = () => {
             initial major of Computer Science. I came to realize that I wasn't
             learning the topics that interested me, and that's when I decided it
             was time to take things into my own hands. As I continued improving
-            my skills as a developer, I completed my degree in Marketing with a
+            my skills as a developer, I completed a degree in Marketing with a
             minor in Computer Science. I am eager to continue learning and
             pursuing a career in this field that I am so passionate about.
           </p>
